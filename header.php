fe48a02b8cda4936130141577dd70dba0e5b63ba
devworkchelmi <dev.chelmi@gmail.com>
2025-02-26 16:59:12 +0100

    create form suscriber

--- header.php
+++ header.php
@@ -7,6 +7,9 @@ require_once './fonctions.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="création-compte.css"><!--css formulaire création de compte-->
+    <link rel="stylesheet" href="header.css"><!--css du header-->
+
     <title>Site boutique</title>
 </head>
 <body>

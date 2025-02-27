3a9f12770b9c46458dec25486102ec76165296c0
Yannick08-cmd <yannick.ranarisoa@gmail.com>
2025-02-27 14:34:46 +0100

    modification fonctions

--- fonctions.php
+++ fonctions.php
@@ -1,13 +1,11 @@
 <?php
 function index(){
-    $nom = "nom de l'utilisateur";
-    $prenom = "prenom de l'utilisateur";
+
     $mail = "adresse mail user";
     $mdp = "mot de passe user";
 
     return [
-        $nom, 
-        $prenom, 
+ 
         $mail,
         $mdp
     ];

45fc6c4825b03467e205637ae5c487c62698b59f
mnopsice <mnopsice@gmail.com>
2025-02-27 17:23:18 +0100

    suppression de setcoockie

--- index.php
+++ index.php
@@ -9,8 +9,6 @@ $identifiant = [$mail, $mdp] = index();
 
 $mail = filter_input(INPUT_COOKIE, "adressemail");  
 
-setcookie($mail, $mdp); // maj de cookie pour un utilisateur
-
     if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['adressemail']) && isset($_POST['mdpuser'])) {
         $mail = filter_input(INPUT_POST, "adressemail", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
         $mdp = filter_input(INPUT_POST, "mdpuser", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
